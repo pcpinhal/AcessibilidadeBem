bc6dead389d22bfad0c9fe14ffc0d39fc4788eed
pcpinhal <pc_pinhal@yahoo.com.br>
2024-05-09 11:52:55 -0300

    criando botao esconder/exibir para o paragrafo

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//teste2
+$(document).ready(function() {
+    $('#botao1').click(function()
+    {
+        $('p').hide('fast');
+    });
+    
+    $('#botao2').click(function()
+    {
+        $('p').show('slow');
+    });
+});
